6de51c1f8481489fcf38d1f01b7c8e2e1c66c9ef
KgbArmageddon <karimydesouzaborda@gmail.com>
2025-01-21 01:23:15 -0300

    Adicionado CatalogoLivrosApplicationTests.java

---
+++ src/test/java/com/literatura/CatalogoLivrosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
