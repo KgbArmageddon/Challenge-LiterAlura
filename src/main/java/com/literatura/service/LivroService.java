74cf7f2d25353be757a82e02f3891134c9234f4a
KgbArmageddon <karimydesouzaborda@gmail.com>
2025-01-21 01:16:37 -0300

    Adicionado LivroService.java

---
+++ src/main/java/com/literatura/service/LivroService.java
@@ -0,0 +1,10 @@
+package com.literatura.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+    public void buscarLivroPorTitulo(String titulo) {
+        // Implementação futura
+    }
+}
